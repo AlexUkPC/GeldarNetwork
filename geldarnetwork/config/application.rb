f3127d549c52e0c820aa6a8402432fbaefad1874
AlexUkPC <alexrogna@yahoo.com>
2021-04-26 13:01:46 +0100

    authentication

--- geldarnetwork/config/application.rb
+++ geldarnetwork/config/application.rb
@@ -18,5 +18,8 @@ module Geldarnetwork
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.to_prepare do
+      Devise::Mailer.layout "mailer"
+    end
   end
 end
