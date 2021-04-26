0ec157eff2a9b49f5b85309aa04751d0b4f50f07
AlexUkPC <alexrogna@yahoo.com>
2021-04-26 11:49:46 +0100

    add models

---
+++ geldarnetwork/app/models/user.rb
@@ -0,0 +1,51 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string           not null
+#  first_name :string           not null
+#  is_public  :boolean          default(TRUE), not null
+#  last_name  :string
+#  username   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email     (email) UNIQUE
+#  index_users_on_username  (username) UNIQUE
+#
+class User < ApplicationRecord
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+  validates :first_name, presence: true
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address"}
+  has_many :bonds
+  has_many :followings,
+  -> { Bond.following },
+  through: :bonds,
+  source: :friend
+
+  has_many :follow_requests,
+  -> { Bond.requesting },
+  through: :bonds,
+  source: :friend
+
+  has_many :inward_bonds,
+  class_name: "Bond",
+  foreign_key: :friend_id
+
+  has_many :followers,
+  -> { Bond.following },
+  through: :inward_bonds,
+  source: :user
+  scope :following, -> { where(state: FOLLOWING)}
+  scope :requesting, -> { where(state: REQUESTING)}
+  scope :blocking, -> { where(state: BLOCKING)}
+  before_save :ensure_proper_name_case
+  private
+  def ensure_proper_name_case
+      self.first_name = first_name.capitalize
+  end
+end
